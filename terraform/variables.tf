b288c17894cf46281a811da219c6e3b3fbef734b
Tristan Wilson <74617725+twilson1401@users.noreply.github.com>
2026-04-08 11:03:04 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,17 @@
+variable "confluent_cloud_api_key" {
+  description = "Confluent Cloud API Key"
+  type        = string
+  sensitive   = true
+}
 
+variable "confluent_cloud_api_secret" {
+  description = "Confluent Cloud API Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "The AWS region for Kafka and Flink"
+  type        = string
+  default     = "us-east-2"
+}
